a2930e3dc01c89a720bae9a000bfb9f2b43c2310
Nicolas Angerossa <nangerossa@frd.utn.edu.ar>
2026-01-02 15:34:58 -0300

    Problemas arreglados, projects terminado

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,10 +3,11 @@ import {Button} from "@/components/button";
 import { BiLogoGithub } from "react-icons/bi";
 import { CgExternal } from "react-icons/cg";
 import { BsArrowLeft } from "react-icons/bs";
-import projects from "./projects.json";
+import Image from "next/image";
+import projectsData from "./projects.json";
 
 export default function Projects() {
-  const {projectsdata} = projects; 
+  const {projects} = projectsData;
   return (
     <div className="min-h-screen flex items-center justify-center">
       <main className="max-w-4xl m-3 md:m-8 z-50 w-full space-y-8 border rounded-xl border-gray-700/50 p-5 sm:p-10 background-blur-xl bg-black/80">
@@ -19,15 +20,48 @@ export default function Projects() {
       </div>  
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        {projectsdata.map((projectsdata) => ( 
-            <div key={projectsdata.id} className="group overflow-hidden border border-gray-700/40 rounded-lg hover:scale-105 border-neutral-800/70 bg-black/50 transition-all duration-300">
-              <div>
-                <Image src= {projectsdata.image} alt={projectsdata.title} 
+        {projects.map((project) => ( 
+            <div key={project.id} className="group overflow-hidden border rounded-lg hover:scale-105 border-neutral-800/70 bg-black/50 transition-all duration-300">
+              <div className="relative w-full h-48 md:h-64 lg:h-48 xl:h-64">
+                <Image src={project.image} alt={project.title} 
                 fill 
-                className="object-cover h-48 w-full group-hover:scale-110 transition-transform transition-all duration-300">
+                className="object-contain transition-transform duration-300 group-hover:scale-110">
                 </Image>
               </div>
-              <div>
+              <div className="p-6">
+                <h3 className="text-xl font-semibold mt-4 ml-4">{project.title}</h3>
+                <p className="text-gray-300 mb-4">{project.description}</p>
+
+                <div className="flex flex-wrap gap-2 mb-4"> 
+                  {project.tags.map((tag, index) => (
+                    <span key={index} className="text-sm bg-slate-800/40 text-gray-300 px-2 py-1 rounded-full border border-neutral-800/70">{tag}</span>
+                  ))}
+                </div>
+                <div className="flex gap-3">
+                  <Button
+                    variant="outline"
+                    className="flex-1 text-gray-300 hover:text-white bg-black backdrop-blur-sm border-neutral-800/70 transition-colors duration-200 ease-linear" asChild
+                  >
+                    <a href={project.link}>
+                      <BiLogoGithub className="h-4 w-4 mr-2" />
+                      Code
+                    </a>
+                  </Button>
+
+                  <Button
+                    variant="ghost"
+                    asChild
+                    className="flex-1 border text-gray-300 hover:text-white backdrop-blur-sm border-neutral-800/70 transition-colors duration-200 ease-linear"
+                  >
+                    <a
+                      href={[1, 2].includes(project.id) ? undefined : project.link}
+                      className={[1, 2].includes(project.id) ? "pointer-events-none opacity-50" : ""}
+                    >
+                      <CgExternal className="h-4 w-4 mr-2" />
+                      Live Demo
+                    </a>
+                  </Button>
+                </div>
 
               </div>
             </div>
